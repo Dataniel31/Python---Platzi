13206707873700020c3d5e5b13633942759df434
Dataniel31 <alexdspe02@gmail.com>
2024-12-31 23:28:48 -0500

    add clase4.py file with string manipulation examples and type demonstrations

---
+++ clase4.py
@@ -0,0 +1,32 @@
+name = "Daniel Alex"
+last_name = " Suclle Luque "
+caracter = 'D'
+# Type nos permite saber el tipo de dato de una variable
+print(type(name))
+print(type(caracter))
+
+# Podemos acceder a los elementos de un string
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+
+print(name + " " +last_name)
+print(name,last_name)
+print(name*5)
+
+# Podemos saber la longitud de un string
+print(len(name))
+print(len(last_name))
+
+
+# Podemos convertir un string a minusculas o mayusculas
+print(name.lower())
+print(name.upper())
+
+# Quitar espacios en blanco al final o al inicio
+print(last_name.strip())
+
