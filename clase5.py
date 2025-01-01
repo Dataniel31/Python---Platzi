b28b8d5a29e846e6b18f00303018fe49eac78069
Dataniel31 <alexdspe02@gmail.com>
2024-12-31 23:38:29 -0500

    add clase5.py file with variable type demonstrations and basic operations

---
+++ clase5.py
@@ -0,0 +1,24 @@
+x = 10
+y = 5.678
+# z = 1.2E6
+# a = 1.2e-6
+print(type(x))
+print(type(y))
+
+# print(z)
+# print(a)
+
+# Operaciones
+print(x + x)
+print(x + y)
+print(y + y)
+
+
+is_true = True
+is_false = False
+
+# 
+print(is_true)
+print(is_false)
+print(type(is_true))
+print(type(is_false))
